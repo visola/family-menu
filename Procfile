ca343b0b556681893fa471666c8f502f48ddc3bd
Vinicius Isola <viniciusisola@gmail.com>
2017-09-28 11:30:44 -0400

    Setup for Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -Dserver.port=$PORT -Dspring.profiles.active="$PROFILES" -Ddatabase.url=$DATABASE_URL $JAVA_OPTS -jar build/libs/family-menu.jar
---
+++ src/main/java/com/github/visola/familymenu/HerokuDatabase.java
@@ -0,0 +1,39 @@
+package com.github.visola.familymenu;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@Profile("heroku")
+public class HerokuDatabase {
+
+    @Value("${database.url}")
+    String databaseUrl;
+
+    @Bean
+    public DataSource dataSource() throws URISyntaxException {
+        URI uri = new URI(databaseUrl);
+
+        String username = uri.getUserInfo().split(":")[0];
+        String password = uri.getUserInfo().split(":")[1];
+        String jdbcUrl = "jdbc:postgresql://" + uri.getHost() + uri.getPath();
+
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(jdbcUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+
+    }
+
+}
