6c8b14c84a2e32de497029b2442f2ed70208e21e
Vinicius Isola <viniciusisola@gmail.com>
2017-10-02 11:30:03 -0400

    Add more coverage for FamilyController

--- src/test/java/com/github/visola/familymenu/controller/FamilyControllerTest.java
+++ src/test/java/com/github/visola/familymenu/controller/FamilyControllerTest.java
@@ -26,22 +26,73 @@ public class FamilyControllerTest {
     private FamilyController familyController;
 
     @Before
-    public void setup() {
+    public void setUp() {
         familyController = new FamilyController(familyRepository, passwordEncoder);
     }
 
     @Test
     public void doesNotExposeFamilyPassword() {
+        Family mockFamily = setupFamilyRepositoryWithOneFamily();
+
+        Family family = familyController.getFamily(1);
+        Assert.assertEquals("Should match ID correctly", mockFamily.getId(), family.getId());
+        Assert.assertEquals("Should match name correctly", mockFamily.getName(), family.getName());
+        MatcherAssert.assertThat(family.getPassword(), RegexMatchers.matchesPattern("\\*+"));
+    }
+
+    @Test(expected=ResourceNotFoundException.class)
+    public void throwsIfFamilyNotFound() {
+        familyController.getFamily(1);
+    }
+
+    @Test(expected=BadRequestException.class)
+    public void throwsIfCreatingFamilyThatAlreadyExist() {
+        setupFamilyRepositoryWithOneFamily();
+
+        Family family = new Family();
+        family.setName("Test");
+        family.setPassword("Another Password");
+
+        familyController.createFamily(family);
+    }
+
+    @Test(expected=BadRequestException.class)
+    public void throwsIfTryingToUpdateFamily() {
+        setupFamilyRepositoryWithOneFamily();
+
+        Family family = new Family();
+        family.setId(1);
+        family.setName("Test");
+        family.setPassword("Another Password");
+
+        familyController.createFamily(family);
+    }
+
+    @Test
+    public void encodePasswordBeforeCreatingFamily() {
+        Family family = new Family();
+        family.setName("Test");
+        family.setPassword("Another Password");
+
+        String encodedPassword = "MyEncodedPassword";
+        Mockito.when(passwordEncoder.encode(family.getPassword())).thenReturn(encodedPassword);
+
+        Mockito.when(familyRepository.save(family)).thenReturn(family);
+
+        Family createdFamily = familyController.createFamily(family);
+        Assert.assertEquals("Should encode password before saving.", encodedPassword, createdFamily.getPassword());
+    }
+
+    private Family setupFamilyRepositoryWithOneFamily() {
         Family mockFamily = new Family();
         mockFamily.setId(1);
         mockFamily.setName("Test");
         mockFamily.setPassword("Some Password");
 
         Mockito.when(familyRepository.findOne(mockFamily.getId())).thenReturn(mockFamily);
+        Mockito.when(familyRepository.findByName(mockFamily.getName())).thenReturn(mockFamily);
 
-        Family family = familyController.getFamily(1);
-        Assert.assertNotEquals(mockFamily.getPassword(), family.getPassword(), "Should not match family password");
-        MatcherAssert.assertThat(family.getPassword(), RegexMatchers.matchesPattern("\\*+"));
+        return mockFamily;
     }
 
 }
