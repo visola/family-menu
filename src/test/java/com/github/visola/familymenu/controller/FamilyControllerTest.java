7a5c1d759ccbf0c438277e7ea55dc2ba453e2668
Vinicius Isola <viniciusisola@gmail.com>
2017-10-02 10:44:01 -0400

    Send test coverage report to Codacy

---
+++ src/test/java/com/github/visola/familymenu/controller/FamilyControllerTest.java
@@ -0,0 +1,13 @@
+package com.github.visola.familymenu.controller;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FamilyControllerTest {
+
+    @Test
+    public void basicTest() {
+        Assert.assertTrue("This will pass.", true);
+    }
+
+}
