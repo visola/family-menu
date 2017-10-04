39b673ea44588118a67097fe7e2ef3be27753249
Vinicius Isola <viniciusisola@gmail.com>
2017-10-04 13:15:21 -0400

    ESLint webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,57 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-let baseConfig = {
+const baseConfig = {
   entry: './src/frontend/js/index.js',
   node: {
-    fs: 'empty'
-  }, output: {
+    fs: 'empty',
+  },
+  output: {
     filename: 'bundle.js',
-    path: __dirname + '/build/frontend/static'
-  }, plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/frontend/html/index.html'
-    })
-  ], module: {
+    path: `${__dirname}/build/frontend/static`,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'src/frontend/html/index.html' }),
+  ],
+  module: {
     loaders: [{
       test: /src\/frontend\/js\/.+\.js$/,
-      loader: "babel-loader"
+      loader: 'babel-loader',
     }, {
       test: /.+\.css$/,
       use: [
-        {loader: "style-loader"}, // creates style nodes from JS strings
-        {loader: "css-loader"}, // translates CSS into CommonJS
-      ]
-    },{
+        { loader: 'style-loader' }, // creates style nodes from JS strings
+        { loader: 'css-loader' }, // translates CSS into CommonJS
+      ],
+    }, {
       test: /src\/frontend\/less\/.+\.less$/,
       use: [
-        {loader: "style-loader"}, // creates style nodes from JS strings
-        {loader: "css-loader"}, // translates CSS into CommonJS
-        {loader: "less-loader"} // compiles Less to CSS
-      ]
-    }]
-  }
-}
+        { loader: 'style-loader' }, // creates style nodes from JS strings
+        { loader: 'css-loader' }, // translates CSS into CommonJS
+        { loader: 'less-loader' }, // compiles Less to CSS
+      ],
+    }],
+  },
+};
 
-module.exports = function (env) {
-  console.log(`Building '${env}' bundle.`);
+module.exports = function exportedConfiguration(env) {
+  const plugins = baseConfig.plugins || [];
+
   switch (env) {
-    case "development":
+    case 'development':
       Object.assign(baseConfig, {
-        devtool: "inline-source-map",
+        devtool: 'inline-source-map',
       });
       break;
-    case "production":
-      let plugins = baseConfig.plugins || [];
-
-      plugins.push(new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify('production')}));
+    case 'production':
+      plugins.push(new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') }));
       plugins.push(new webpack.optimize.UglifyJsPlugin());
 
-      Object.assign(baseConfig, {plugins: plugins});
+      Object.assign(baseConfig, { plugins });
+      break;
+
+    default:
+      throw new Error('Unhandled environment.');
   }
 
   return baseConfig;
