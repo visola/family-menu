928888334247f1455375c5db5dd23265a100394e
Vinicius Isola <viniciusisola@gmail.com>
2017-10-10 13:53:40 -0400

    Add integration test infrastructure

---
+++ src/integration-test/java/com/github/visola/familymenu/controller/FamilyMenuControllerIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.github.visola.familymenu.controller;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FamilyMenuControllerIntegrationTest {
+
+    @Test
+    public void passingTest() {
+        Assert.assertTrue("This will pass", true);
+    }
+
+}
