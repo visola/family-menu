0f640fe56f143cd457454f1e3681685578387c58
Vinicius Isola <viniciusisola@gmail.com>
2017-10-13 15:48:29 -0400

    Refactor frontend to handle routes correctly

---
+++ src/frontend/js/components/PageNotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class PageNotFound extends React.Component {
+  render() {
+    return <p className="page-not-found">Sorry but I can't find that page.</p>;
+  }
+}
+
+export default PageNotFound;
