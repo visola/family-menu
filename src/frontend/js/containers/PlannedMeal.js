eca7a782da3f272bc2c8923d1a094444451f9826
Vinicius Isola <viniciusisola@gmail.com>
2017-10-23 09:37:55 -0400

    Setup PlannedMeal component to receive a list

--- src/frontend/js/containers/PlannedMeal.js
+++ src/frontend/js/containers/PlannedMeal.js
@@ -2,12 +2,17 @@ import Button from 'react-bootstrap/lib/Button';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
+import { Minus, Plus } from 'react-bytesize-icons';
 
 class PlannedMeal extends React.Component {
   render() {
     return <div className="meal">
-      {this.props.person.name}
-      <div><Button>Add</Button></div>
+      {this.props.person.name} <Button><Plus height="16" width="16" /></Button>
+      <ul>
+        <li>Rice<Button><Minus height="16" width="16" /></Button></li>
+        <li>Bean<Button><Minus height="16" width="16" /></Button></li>
+        <li>Grilled chicken<Button><Minus height="16" width="16" /></Button></li>
+      </ul>
     </div>;
   }
 }
